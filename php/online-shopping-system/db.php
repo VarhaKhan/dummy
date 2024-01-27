662794f109a817746cc2aed603675f9ea0beb8d1
Varha Khan <varhakhan098@gmail.com>
2024-01-26 19:04:01 -0500

    Website error at Localhost

--- php/online-shopping-system/db.php
+++ php/online-shopping-system/db.php
@@ -6,12 +6,17 @@ $password = "admin";
 $db = "database";
 
 // Create connection
-$con = mysqli_connect($servername,$username,$password,$db);
+$con = mysqli_connect($servername, $username, $password, $db);
 
 // Check connection
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully!";
 }
 
+// Close the connection
+mysqli_close($con);
 
 ?>
+
